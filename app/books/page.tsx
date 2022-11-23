5b7ab9602ad141ef9bf1067a39bd06019e51ebba
Per Hansen <pekkelwebsites@gmail.com>
2022-11-23 15:15:11 +0100

    responsive books

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -58,7 +58,7 @@ function TierSection({ title, books }: { title: string; books: Book[] }) {
     <div className="relative flex">
       <div className="flex flex-col w-full pl-4 sm:pl-8">
         <h2 className="pl-5 text-xl font-extrabold border-l-2 border-white">{title}</h2>
-        <div className="grid grid-cols-3 gap-2 mt-5">
+        <div className="grid grid-cols-1 gap-2 mt-5 md:grid-cols-2 lg:grid-cols-3">
           {books.map((book, i) => (
             <div key={i} className="flex flex-col p-6 rounded bg-white/5">
               <h3 className="font-medium leading-4">{book.name}</h3>
