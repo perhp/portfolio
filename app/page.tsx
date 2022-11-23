3734588ff7a6c851c6bc51dbf3633afb0b83d394
Per Hansen <pekkelwebsites@gmail.com>
2022-11-23 15:16:20 +0100

    author on frontpage aswell

--- app/page.tsx
+++ app/page.tsx
@@ -121,6 +121,7 @@ export default async function Page() {
               <div className="flex flex-col w-full">
                 <p className="text-sm">Currently Reading</p>
                 <p className="text-3xl font-medium">How to Avoid a Climate Disaster</p>
+                <p className="mt-1 text-sm text-slate-300">by Bill Gates</p>
               </div>
             </div>
 
