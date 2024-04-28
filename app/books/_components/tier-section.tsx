3a190e9c5185fe30e2f6c1211f66c0b2c4501899
Per Hansen <me@perhp.com>
2024-04-28 18:18:25 +0200

    feat: add search links for books

--- app/books/_components/tier-section.tsx
+++ app/books/_components/tier-section.tsx
@@ -6,12 +6,17 @@ export default function TierSection({ title, books }: { title: string; books: Bo
       <div className="flex flex-col w-full">
         <h2 className="pl-5 text-xl font-extrabold border-l-2 border-white">{title}</h2>
         <div className="grid grid-cols-1 gap-2 mt-5 md:grid-cols-2 lg:grid-cols-3">
-          {books.map((book, i) => (
-            <div key={i} className="flex flex-col p-6 rounded bg-white/5">
-              <h3 className="font-medium leading-4">{book.name}</h3>
-              <p className="mt-1 text-xs text-slate-300">by {book.author}</p>
-            </div>
-          ))}
+          {books.map((book, i) => {
+            const link = new URL(`https://www.google.com/search`);
+            link.searchParams.append("q", `${book.name} by ${book.author}`);
+
+            return (
+              <a key={i} href={link.toString()} target="_blank" className="flex flex-col p-6 rounded bg-white/5">
+                <h3 className="font-medium leading-4">{book.name}</h3>
+                <p className="mt-1 text-xs text-slate-300">by {book.author}</p>
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
