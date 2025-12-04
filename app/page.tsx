b981b87866b4d54bef59331749f00b6a740a9724
Per Hansen <me@perhp.com>
2025-12-04 22:17:13 +0100

    fix: signature animation

--- app/page.tsx
+++ app/page.tsx
@@ -35,10 +35,10 @@ export default function Page() {
             Hire me <ArrowTopRightOnSquareIcon className="w-5 h-5" />
           </a>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 596 289" fill="none" className="pb-3 border-b border-white mt-14 max-h-20">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 596 289" fill="none" className="pb-1 border-b border-white mt-14 max-h-20">
           <path
             d="M178 31C169.894 38.6363 163.15 47.2952 156.222 56C141.318 74.7283 126.52 93.5416 111.556 112.222C89.7418 139.454 67.6258 166.439 45 193"
-            stroke="black"
+            stroke="white"
             strokeWidth="5"
             strokeLinecap="round"
             className={styles.signature_1}
